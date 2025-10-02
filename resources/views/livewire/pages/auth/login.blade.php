e01f1888f5b8d40fddc5ef6e56f7593114cf45a2
taha <t.mousnia@factory.gr>
2025-10-02 19:58:16 +0100

    modify login button

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -5,8 +5,7 @@ use Illuminate\Support\Facades\Session;
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
-new #[Layout('layouts.guest')] class extends Component
-{
+new #[Layout('layouts.guest')] class extends Component {
     public LoginForm $form;
 
     /**
@@ -32,7 +31,8 @@ new #[Layout('layouts.guest')] class extends Component
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="form.email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus autocomplete="username" />
+            <x-text-input wire:model="form.email" id="email" class="block mt-1 w-full" type="email" name="email"
+                required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('form.email')" class="mt-2" />
         </div>
 
@@ -40,38 +40,54 @@ new #[Layout('layouts.guest')] class extends Component
         <div class="mt-4">
             <x-input-label for="password" :value="__('Mot de passe')" />
 
-            <x-text-input wire:model="form.password" id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <x-text-input wire:model="form.password" id="password" class="block mt-1 w-full" type="password"
+                name="password" required autocomplete="current-password"/>
 
             <x-input-error :messages="$errors->get('form.password')" class="mt-2" />
+
         </div>
 
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember" class="inline-flex items-center">
-                <input wire:model="form.remember" id="remember" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <input wire:model="form.remember" id="remember" type="checkbox"
+                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ms-2 text-sm text-gray-600">{{ __('Se souvenir de moi') }}</span>
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-left mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}" >
-                    {{ __('Mot de passe oublié?
-') }}
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    href="{{ route('password.request') }}">
+                    {{ __('Mot de passe oublié?') }}
                 </a>
             @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Se connecter') }}
-            </x-primary-button>
-            
         </div>
-        <a href="{{route('auth.google')}}" class="w-full flex justify-center sm:px-0">
-            <button type="button" class="text-white w-full  bg-emerald-600 hover:bg-emerald-600/90 focus:ring-4 focus:outline-none focus:ring-emerald-600/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between  mt-2"><svg class=" w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>Se connecter avec Google<div></div></button>
+        <x-primary-button>
+            {{ __('Se connecter') }}
+        </x-primary-button>
+        <div class="w-full text-center inline-flex items-center justify-center mt-2"><span> {{ __('Ou') }}</span>
+        </div>
+        <a href="{{ route('auth.google') }}" class="w-full flex justify-center sm:px-0">
+            <button type="button"
+                class="text-gray w-full bg-white hover:bg-blue-300/50 focus:ring-4 focus:outline-none focus:ring-emerald-600/50 font-medium rounded-lg  border-2 border-gray-300 border-solid text-sm px-5 py-2.5 text-center inline-flex items-center justify-between mt-2">
+                <img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy"
+                    alt="google logo">
+                Se connecter avec Google<div></div></button>
         </a>
     </form>
 
 </div>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const passIn = document.getElementById('password');
+        const btn = document.getElementById('togglePassword');
+        btn.addEventListener('click', function() {
+            const type =
+                passIn.getAttribute('type') ===
+                'password' ? 'text' : 'password';
+            passIn.setAttribute('type', type);
+        });
+    });
+</script>
